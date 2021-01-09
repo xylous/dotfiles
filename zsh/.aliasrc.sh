1e24af68f53536ed91baf9623ebf931a5d92590a
xylous <xylous.e@gmail.com>
2021-01-09 11:41:48 +0200

    Added comments to .aliasrc.sh

--- zsh/.aliasrc.sh
+++ zsh/.aliasrc.sh
@@ -4,17 +4,32 @@
 
 alias ls='ls --color=auto'
 
-# Saves quite a bit of time
-alias v='\nvim'
+# Saves a lot of time
+if command -v nvim &> /dev/null; then
+    alias v='\nvim'
+elif command -v vim &> /dev/null; then
+    alias v='\vim'
+fi
 
-# Quick access to config files
-alias cfz='nvim ~/.zshrc'
+## Quick access to config files
+# "configure z-shell"
+alias cfzs='nvim ~/.zshrc'
+# "configure bash"
+alias cfbs='nvim ~/.bashrc'
+# "configure shell aliases"
 alias cfsa='nvim ~/.aliasrc.sh'
-alias cfv='nvim ~/.config/nvim/init.vim'
-alias cfi='nvim ~/.config/i3/config'
-alias cfpi='nvim ~/.config/picom.conf'
-alias cfa='nvim ~/.config/alacritty/alacritty.yml'
+# "configure neovim"
+alias cfnv='nvim ~/.config/nvim/init.vim'
+# "configure i3wm"
+alias cfi3='nvim ~/.config/i3/config'
+# "configure picom"
+alias cfpic='nvim ~/.config/picom.conf'
+# "configure alacritty"
+alias cfal='nvim ~/.config/alacritty/alacritty.yml'
+# "configure polybar"
+alias cfpy='nvim ~/.config/polybar/config'
 
 # Package manager
 alias p='pacman'
 alias sp='sudo pacman'
+alias y='yay'
