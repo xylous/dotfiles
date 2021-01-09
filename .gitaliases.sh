5aec8f00c45f807bb50437a0156c54450af98c24
xylous <xylous.e@gmail.com>
2021-01-09 18:56:06 +0200

    Added aliases for git

---
+++ .gitaliases.sh
@@ -0,0 +1,13 @@
+# Time savers
+alias ga="git add"
+
+alias gpl="git pull"
+
+alias gpsh="git push"
+
+alias gs="git status"
+alias gss="git status --short"
+
+alias gcm="git commit"
+alias gcmm="git commit -m"
+alias gcma="git commit --all" 
